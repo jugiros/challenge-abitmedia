6c5170cba0cf243c0da19b3cfceab9c2a0917f87
jugiros <31550758+jugiros@users.noreply.github.com>
2023-09-02 01:22:26 -0500

    feat: Creación proyecto back-end API/REST.

---
+++ abitmedia/src/main/java/com/org/abitmedia/AbitmediaApplication.java
@@ -0,0 +1,13 @@
+package com.org.abitmedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbitmediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbitmediaApplication.class, args);
+	}
+
+}
---
+++ abitmedia/src/test/java/com/org/abitmedia/AbitmediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.abitmedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbitmediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
