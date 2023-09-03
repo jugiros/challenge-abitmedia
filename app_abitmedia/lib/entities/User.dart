d440b492ba19c08ab0957e89e72649215ca8e9d0
jugiros <31550758+jugiros@users.noreply.github.com>
2023-09-02 23:24:25 -0500

    feat: Integración Entidad User para validar los datos de un usuario.

---
+++ app_abitmedia/lib/entities/User.dart
@@ -0,0 +1,27 @@
+class User {
+  final String name;
+  final String lastName;
+  final String email;
+  final String password;
+
+  User(this.name, this.lastName, this.email, this.password);
+
+  Map<String, dynamic> toJson() {
+    return {
+      'name': name,
+      'lastName': lastName,
+      'email': email,
+      'password': password
+    };
+  }
+
+  factory User.fromJson(dynamic json) {
+    return User(json['name'] as String, json['lastName'] as String,
+        json['email'] as String, json['password'] as String);
+  }
+
+  @override
+  String toString() {
+    return '{ $name, $lastName, $email, $password }';
+  }
+}
