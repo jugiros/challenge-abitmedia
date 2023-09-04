c17e89d3aaf5e96ec88837ddc8421d3162ac93c8
jugiros <31550758+jugiros@users.noreply.github.com>
2023-09-04 00:26:46 -0500

    feat: Integración controlador para generar una solicitud de pagos.

---
+++ abitmedia/src/main/java/com/org/abitmedia/controller/PagosMediosController.java
@@ -0,0 +1,30 @@
+package com.org.abitmedia.controller;
+
+import com.org.abitmedia.pagosmedios.PagosMedios;
+import com.org.abitmedia.pagosmedios.payload.RequestData;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api/v1")
+public class PagosMediosController {
+
+    @Autowired
+    private PagosMedios pagosMedios;
+
+    @PostMapping(value = "/payment-request")
+    @PreAuthorize("hasAnyAuthority('STANDAR_USER')")
+    public ResponseEntity<?> PaymentRequest(
+            @Valid @RequestBody RequestData requestData
+    ) {
+        return pagosMedios.PaymentRequest(requestData);
+    }
+
+}
